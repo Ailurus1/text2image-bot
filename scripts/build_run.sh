d71d8fe85d423b8989da1b22eab1ee28cd75033f
Ailurus1 <kozulin02@yandex.ru>
2023-06-05 09:20:46 +0300

    Update dockerignore and build_run script

--- scripts/build_run.sh
+++ scripts/build_run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
+
 cd ..
-docker build \
-    --build-arg TG_TOKEN_ARG=$TG_TOKEN \
-    --build-arg HF_TOKEN_ARG=$HF_TOKEN \
-    -t text2image-bot:latest .
-docker run text2image-bot:latest
+docker build -t text2image-bot:latest .
+docker run --env-file=.env -it text2image-bot:latest
